85b96c6778476c7a7e140bf0a4dd4d221325494b
Ankush Mishra <ankushmishra0939@gmail.com>
2020-11-01 11:07:31 +0530

    init

---
+++ script.js
@@ -0,0 +1,11 @@
+function textChanger() {
+    document.getElementById('textChange').innerHTML='And this is the changed text!!'
+}
+
+function attributeChanger() {
+    document.getElementById('attributeChange').src='img/ch.png'
+}
+
+function styleChanger() {
+    document.getElementById('styleChange').style.color='red'
+}
