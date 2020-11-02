30fb2c8c02e5d20164f42da39daed3242ee23aea
Ankush Mishra <ankushmishra0939@gmail.com>
2020-11-02 11:29:51 +0530

    Date Formats

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ function styleChanger() {
 function popup() {
     window.alert("This is how pop-ups are made")
 }
+
+d = new Date()
+function viewdate() {
+    document.getElementById('datetext1').style.display='block';
+    document.getElementById('viewdate1').innerHTML=d.toString()
+    document.getElementById('datetext2').style.display='block';
+    document.getElementById('viewdate2').innerHTML=d.toUTCString()
+    document.getElementById('datetext3').style.display='block';
+    document.getElementById('viewdate3').innerHTML=d.toDateString()
+    document.getElementById('datetext4').style.display='block';
+    document.getElementById('viewdate4').innerHTML=d.toISOString()
+}
